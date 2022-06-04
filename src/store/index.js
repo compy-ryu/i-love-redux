e79c9efda4bd47297f9134abe903edd9c9ef8be1
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-06-05 01:55:26 +0900

    package: thunk 설치

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "redux-thunk";
 import ReduxLogger from "redux-logger";
 
 import { commonReducer } from "./common";
 
 const reducers = combineReducers({ common: commonReducer });
 
-export default createStore(reducers, composeWithDevTools(applyMiddleware(ReduxLogger)));
+export default createStore(reducers, composeWithDevTools(applyMiddleware(ReduxThunk, ReduxLogger)));
